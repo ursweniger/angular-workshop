3c954d21146ecfd0d388e304a645c64c8e49a59b
hf018 <hf018@hdm-stuttgart.de>
2024-06-17 21:51:35 +0200

    add cors, change logger

--- Aufgabex3/hdm-events-backend/src/logger/logger.middleware.ts
+++ Aufgabex3/hdm-events-backend/src/logger/logger.middleware.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request Headers: ', req.headers)
+    console.log('INFO ', req.ip, req.originalUrl, req.method, req.body)
     next();
   }
 }
